8b87cb2bfa75191ba1b8293508e55fb23499b4ed
Leonardo Tricotti <tricottileo@gmail.com>
2023-12-06 09:11:21 -0300

    commit

--- js/newPassword.scripts.js
+++ js/newPassword.scripts.js
@@ -114,3 +114,8 @@ const showNewPassword = () => {
     ? "text"
     : "password";
 };
+
+// Spinner de carga
+const btnSpinner = () => {
+  loginButton.innerHTML = `<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span> Cargando...`;
+};
